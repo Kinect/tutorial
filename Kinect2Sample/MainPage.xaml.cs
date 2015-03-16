eff7c3e1ac3c0c0aac74c40e4bca3f452039f1d5
jesse-nsquared <jesse@nsquaredsolutions.com>
2015-03-16 17:24:42 +1100

    lab01

---
+++ Kinect2Sample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WindowsPreview.Kinect;
+
+namespace Kinect2Sample
+{
+
+    public sealed partial class MainPage : Page
+    {
+
+        private KinectSensor kinectSensor = null;
+
+        public MainPage()
+        {
+            // one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            this.InitializeComponent();
+        }
+    }
+}
